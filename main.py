bf2e549ee20e15b0cf567ad3c06e920e5d983123
Lucas Stevenson <45473315+LucasStevenson@users.noreply.github.com>
2022-05-30 14:52:13 -0700

    reduced runtime by 14 seconds by mapping colleges to players instead of players to colleges after scraping the site

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-import csv
-import os
+import os, sys, csv, ssl, certifi
 from collections import defaultdict
-import sys
 from requests import get
 from bs4 import BeautifulSoup
 import pandas as pd
 import plotly.graph_objects as go
 from geopy.geocoders import Nominatim
 import geopy.geocoders
-import ssl
-import certifi
 
 class Main():
     def __init__(self, csvFile):
         self.csvFile = csvFile
 
-    def _getPlayersAndColleges(self):
+    def _getRosterData(self):
+        # scrapes the roster webpage and returns the table rows that have each player's information
         url = "https://www.49ers.com/team/players-roster/"
         response = get(url)
         nfl = BeautifulSoup(response.content, 'html.parser')
@@ -23,14 +20,20 @@ class Main():
         nfl_roster = nfl_main.find(summary="Roster")
         nfl_roster_body = nfl_roster.find('tbody')
         rows = nfl_roster_body.find_all('tr')
-        playersToCollege = {}
+        return rows
+
+    def _map_college_to_players(self):
+        # maps college names to all the players that went to that college
+        rows = self._getRosterData()
+        college_to_player = defaultdict(list)
         for row in rows:
-            playerName = row.find_all('td')[0].text
-            playerCollege = row.find_all('td')[7].text
-            playersToCollege[playerName.strip()] = playerCollege.strip()
-        return playersToCollege
+            name = row.find_all('td')[0].text.strip()
+            college = row.find_all('td')[7].text.strip()
+            college_to_player[college].append(name)
+        return college_to_player
 
     def _get_coords(self, uni):
+        # gets the latitude and longitude of university 
         ctx = ssl.create_default_context(cafile=certifi.where())
         geopy.geocoders.options.default_ssl_context = ctx
         geolocator = Nominatim(user_agent='my_map')
@@ -43,18 +46,23 @@ class Main():
         return geolocator.geocode(f"University of {uni}")
 
     def _map_location_to_player(self):
-        lat_long_dict = defaultdict(list)
-        players_uni_dict = self._getPlayersAndColleges()
-        for player, university in players_uni_dict.items():
+        # Creates dictionary that maps [latitude, longitude, collegeName] to players that went to that college
+        # Example: {[37.8, -122.2, "Berkeley"]: [players that went to Berkeley]}
+        college_to_player = self._map_college_to_players()
+        lat_long_dict = {}
+        for college, players in college_to_player.items():
             try:
-                location = self._get_coords(university)
-                lat_long_dict[(location.latitude, location.longitude, university)].append(player)
+                location = self._get_coords(college)
+                lat_long_dict[(location.latitude, location.longitude, college)] = players
             except Exception as e:
                 print(e)
                 continue
         return lat_long_dict
 
+
     def _write_data_to_csvFile(self):
+        # writes data to csv file
+        # Format: [Players that went to this college], Latitude of college, Longitude of college, college name
         lat_long_dict = self._map_location_to_player()
         with open(self.csvFile, 'w') as player_file:
             player_writer = csv.writer(player_file, delimiter=',')
@@ -65,6 +73,7 @@ class Main():
                     [players, latitude, longitude, college])
 
     def _plot_data(self):
+        # Reads the csv file and plots data on a US map
         df = pd.read_csv(self.csvFile)
         print(df)
         fig = go.Figure(data=go.Scattergeo(
